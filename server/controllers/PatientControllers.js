85a9ca76e22ca34864c341da6ceb4a0abb7f8e12
Akesandra <bcsandra000@gmail.com>
2021-02-02 18:08:41 +0200

    first commit

--- server/controllers/PatientControllers.js
+++ server/controllers/PatientControllers.js
@@ -9,3 +9,54 @@ export const createPatient = async (req,res)=>{
         newPatient
     })
 }
+export const getPatient=(req,res)=>{
+    patientInfos.findById(req.params.id,(err,patientInfos)=>{
+        if(err){
+            //console.log(err)
+        }
+        else{
+            console.log(patientInfos)
+            res.status(200).json({
+                status:"success",patientInfos
+            })
+        }
+    })
+
+}
+export const updatePatient=(req,res)=>{
+    let patient={};
+    patient.patientName=req.body.patientName;
+    patient.phone=req.body.phone;
+    patient.home=req.body.home;
+    patient.email=req.body.email;
+    let querry={_id:req.params.id}
+    patientInfos.updateOne(querry,patient,(err)=>{
+        if(err){
+            console.log(err)
+            return;
+        }else{
+            res.status(201).json({
+                status:"updated succesfully",
+                patientInfos
+            })
+        }
+    })
+}
+
+export const deletePatient=(req,res)=>{
+    console.log('Deleted')
+    let querry= {_id:req.params.id}
+    patientInfos.deleteOne(querry,(err)=>{
+        if(err){
+            console.log(err)
+        }else console.log('success');
+        res.send('deleted successfully')
+    })
+}
+export const getAllPatient=(req,res)=>{
+    patientInfos.find({}).then((patients)=>{
+        res.send(patients)
+    }).catch((e) =>{
+        res.status(500).srnd(e)
+    })
+}
--- server/routes/PatientRoute.js
+++ server/routes/PatientRoute.js
@@ -4,5 +4,8 @@ import * as patientControl from "../controllers/PatientControllers";
 const patientRouter = express.Router();
 
 patientRouter.post("/create",patientControl.createPatient);
-
+patientRouter.get("/read/:id",patientControl.getPatient);
+patientRouter.post("/edit/:id",patientControl.updatePatient)
+patientRouter.delete("/delete/:id",patientControl.deletePatient)
+patientRouter.get("/getAll",patientControl.getAllPatient)
 export default patientRouter;
